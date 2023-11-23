d741d1c3d8fb8b0b14dbca0e304d3bdec12f7a0c
patamarcelo <patamarcelo@gmail.com>
2023-11-23 11:57:01 -0300

    adjust localhost

--- utils/axios/axios.utils.js
+++ utils/axios/axios.utils.js
@@ -1,10 +1,10 @@
 import axios from "axios";
 
 const baseURL = "https://pitaya-server.herokuapp.com/";
-const baseURLdev = "http://localhost:5500/";
+const baseURLdev = "http://192.168.0.156:5500/";
 
 export const createClient = axios.create({
-	baseURL: process.env.NODE_ENV !== "production" ? baseURL : baseURL,
+	baseURL: process.env.NODE_ENV !== "production" ? baseURLdev : baseURLdev,
 	headers: {
 		"Content-Type": "application/json"
 	}
