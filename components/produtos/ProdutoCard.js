e44444fb6021352ee9b8db2a4ffe751864a58bb1
patamarcelo <patamarcelo@gmail.com>
2025-03-27 18:01:48 -0300

    changing how show prods pic

--- components/produtos/ProdutoCard.js
+++ components/produtos/ProdutoCard.js
@@ -3,9 +3,14 @@ import React from 'react'
 import * as Haptics from 'expo-haptics';
 import { Colors } from '../../constants/styles';
 import { Image } from 'expo-image';
+import { useState } from 'react';
 
 
-const ProdutoCard = ({ item,  setSelectProdsList, selectProdsList }) => {
+const defaultImage = require("../../assets/no-image.png");
+const ProdutoCard = ({ item, setSelectProdsList, selectProdsList }) => {
+
+    const [error, setError] = useState(false);
+
     const formaCurrency = (number) => {
         return number.toLocaleString(
             "pt-br",
@@ -15,13 +20,13 @@ const ProdutoCard = ({ item,  setSelectProdsList, selectProdsList }) => {
             })
     }
 
-    
+
 
     const handlePress = (product) => {
         Haptics.impactAsync(Haptics.ImpactFeedbackStyle.Heavy)
         setSelectProdsList((prevList) => {
             const exists = prevList.find((p) => p.pk === product.pk);
-    
+
             if (exists) {
                 // Remove the product if it exists
                 return prevList.filter((p) => p.pk !== product.pk);
@@ -32,33 +37,49 @@ const ProdutoCard = ({ item,  setSelectProdsList, selectProdsList }) => {
         });
     }
 
+    const blurhash =
+    'LID9#O9F00~qM{M{oft700-;_39F'
+//   '|rF?hV%2WCj[ayj[a|j[az_NaeWBj@ayfRayfQfQM{M|azj[azf6fQfQfQIpWXofj[ayj[j[fQayWCoeoeaya}j[ayfQa{oLj?j[WVj[ayayj[fQoff7azayj[ayj[j[ayofayayayj[fQj[ayayj[ayfjj[j[ayjuayj[';
+
+
     return (
         <Pressable
             style={({ pressed }) => [
                 styles.mainContainer,
                 pressed && styles.pressed,
-                { backgroundColor: selectProdsList?.some((p) => p.pk === item.pk) ? 'rgba(153,204,153,1)' : 'white' }, 
-                { border: selectProdsList?.some((p) => p.pk === item.pk) ? 'green' : 'white' }, 
-                { borderWidth: selectProdsList?.some((p) => p.pk === item.pk) ? 1 : 0 }, 
+                { backgroundColor: selectProdsList?.some((p) => p.pk === item.pk) ? 'rgba(153,204,153,1)' : 'white' },
+                { border: selectProdsList?.some((p) => p.pk === item.pk) ? 'green' : 'white' },
+                { borderWidth: selectProdsList?.some((p) => p.pk === item.pk) ? 1 : 0 },
             ]}
             onPress={() => handlePress(item)}
         >
             {/* Left: Product Image */}
-            <Image 
+            <Image
+                source={{
+                    uri: error ? defaultImage : item?.imageUrl, // If error, set to empty string, fallback to default image
+                    cache: 'force-cache'  // Apply cache here
+                }}
+                contentFit="cover"
+                placeholder={{ blurhash }}
+                transition={250}
+                style={styles.image}
+                onError={() => setError(true)}  // Set error flag on image load failure
+            />
+            {/* <Image 
                 source={item?.imageUrl ? { uri: item?.imageUrl } : require('../../assets/no-image.png')}  
                 style={styles.image} 
-            />
+            /> */}
 
             {/* Right: Product Info */}
             <View style={styles.textContainer}>
                 <Text style={styles.productId}>{item.product_id_produto}</Text>
-                <Text style={[styles.model,{ color: selectProdsList?.some((p) => p.pk === item.pk) ? 'white' : '#2A9D8F'  }]}>
-                {item.content_type__model?.charAt(0).toUpperCase() + item?.content_type__model?.slice(1)} {item.product_tamanho ? " - " + item.product_tamanho : ''}
-                    </Text>
+                <Text style={[styles.model, { color: selectProdsList?.some((p) => p.pk === item.pk) ? 'white' : '#2A9D8F' }]}>
+                    {item.content_type__model?.charAt(0).toUpperCase() + item?.content_type__model?.slice(1)} {item.product_tamanho ? " - " + item.product_tamanho : ''}
+                </Text>
             </View>
 
             {/* Right: Price */}
-            <Text style={[styles.price,{ color: selectProdsList?.some((p) => p.pk === item.pk) ? 'white' : '#2A9D8F'  }, ]}>R$ {formaCurrency(item.sell_price)}</Text>
+            <Text style={[styles.price, { color: selectProdsList?.some((p) => p.pk === item.pk) ? 'white' : '#2A9D8F' },]}>R$ {formaCurrency(item.sell_price)}</Text>
         </Pressable>
     )
 }
--- utils/axios/axios.utils.js
+++ utils/axios/axios.utils.js
@@ -4,7 +4,7 @@ import {
 } from "@env";
 
 const baseURL = "https://pitaya-server.herokuapp.com/";
-const baseURLdev = "http://192.168.0.232:8080/"; // Use your current local IP
+const baseURLdev = "http://192.168.1.108:8080/"; // Use your current local IP
 const baseURLdevDjango = "https://pitaya-django-pay.up.railway.app/"; // Use your current local IP
 
 export const createClient = axios.create({
@@ -14,7 +14,7 @@ export const createClient = axios.create({
 	}
 });
 export const createDjangoClient = axios.create({
-	baseURL: process.env.NODE_ENV !== "production" ? baseURLdevDjango : baseURLdevDjango,
+	baseURL: process.env.NODE_ENV !== "production" ? baseURLdev : baseURLdevDjango,
 	headers: {
 		"Content-Type": "application/json",
 		'Authorization': `Token ${EXPO_PUBLIC_REACT_APP_DJANGO_TOKEN}`
