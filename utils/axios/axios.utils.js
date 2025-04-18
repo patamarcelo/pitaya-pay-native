9cdfe8f8e70fb0e3b77c31797b27b74c1dcb9224
patamarcelo <patamarcelo@gmail.com>
2025-04-18 17:58:02 -0300

    changed heroku to railway

--- utils/axios/axios.utils.js
+++ utils/axios/axios.utils.js
@@ -3,7 +3,10 @@ import {
 	EXPO_PUBLIC_REACT_APP_DJANGO_TOKEN
 } from "@env";
 
-const baseURL = "https://pitaya-server.herokuapp.com/";
+
+// const baseURL = "https://pitaya-server.herokuapp.com/";
+
+const baseURL = "https://pitaya-pay-serverjs-production.up.railway.app/";
 const baseURLdev = "http://192.168.1.108:8080/"; // Use your current local IP
 const baseURLdevDjango = "https://pitaya-django-pay.up.railway.app/"; // Use your current local IP
 
@@ -14,7 +17,7 @@ export const createClient = axios.create({
 	}
 });
 export const createDjangoClient = axios.create({
-	baseURL: process.env.NODE_ENV !== "production" ? baseURLdev : baseURLdevDjango,
+	baseURL: process.env.NODE_ENV !== "production" ? baseURLdevDjango : baseURLdevDjango,
 	headers: {
 		"Content-Type": "application/json",
 		'Authorization': `Token ${EXPO_PUBLIC_REACT_APP_DJANGO_TOKEN}`
